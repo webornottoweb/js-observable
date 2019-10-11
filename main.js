7910b1aa102aa5a2be0d3559a12803fe7b1196f8
Andrey Palamarchuk <a.palamarchuk@corp.101xp.com>
2019-10-11 16:23:21 +0300

    Observer functionality wrapped within function

--- main.js
+++ main.js
@@ -1,59 +1,70 @@
 let data = {
-    firstName: 'Jon',
+    firstName: 'John',
     lastName: 'Snow',
     age: 25
 }
 
-function makeReactive(obj, key) {
-    let val = obj[key];
+const observer = new Observer(data);
+let newData = observer.data;
 
-    Object.defineProperty(obj, key, {
-        get() {
-            return val;
-        },
-        set(newVal) {
-            val = newVal;
-            console.log(newVal);
-            notify(key, newVal);
-        }
-    });
-}
-
-function observeData(obj) {
-    for (let key in obj) {
-        if (obj.hasOwnProperty(key)) {
-            makeReactive(obj, key);
-        }
-    }
-}
-
-observeData(data);
-
-let signals = {};
-
-function observe(property, callback) {
-    if (!signals[property]) {
-        signals[property] = [];
-    }
-
-    signals[property].push(callback);
-}
-
-function notify(signal, newVal) {
-    if (!signals[signal] || signals[signal].length < 1) {
-        return;
-    }
-
-    signals[signal].forEach(callback => callback(newVal));
-}
-
-observe('firstName', newVal => {
+observer.observe('firstName', newVal => {
     console.log(`Oh no, first name was changed with ${newVal}`);
 });
 
-observe('lastName', newVal => {
+observer.observe('lastName', newVal => {
     console.log(`Yeah, last name was changed with ${newVal}`);
 });
 
-data.firstName = 'New';
-data.lastName = 'New1';
+newData.firstName = "Hola";
+newData.lastName = "No no no";
+
+function Observer(dataObj) {
+    let signals = {};
+
+    observeData(dataObj);
+
+    return {
+        data: dataObj,
+        observe,
+        notify
+    }
+
+    function makeReactive(obj, key) {
+        let val = obj[key];
+
+        Object.defineProperty(obj, key, {
+            get() {
+                return val;
+            },
+            set(newVal) {
+                val = newVal;
+                console.log(newVal);
+                notify(key, newVal);
+            }
+        });
+    }
+
+    function observeData(obj) {
+        for (let key in obj) {
+            if (obj.hasOwnProperty(key)) {
+                makeReactive(obj, key);
+            }
+        }
+    }
+
+    function observe(property, callback) {
+        if (!signals[property]) {
+            signals[property] = [];
+        }
+
+        signals[property].push(callback);
+    }
+
+    function notify(signal, newVal) {
+        if (!signals[signal] || signals[signal].length < 1) {
+            return;
+        }
+
+        signals[signal].forEach(callback => callback(newVal));
+    }
+}
